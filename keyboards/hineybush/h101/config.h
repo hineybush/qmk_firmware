8abbd7c90ebfa9f4f6671b038808d97d16e4380e
hineybush <joshhinnebusch@gmail.com>
2023-06-20 23:38:47 -0400

    initial commit

---
+++ keyboards/hineybush/h101/config.h
@@ -0,0 +1,7 @@
+// Copyright 2023 Josh Hinnebusch (@hineybush)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
---
+++ keyboards/hineybush/h101/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x05, 0xFB, 0xF6, 0x56, 0xA8, 0x99, 0x02, 0x6A}
+
+/* CONSIDER ADDING AN UNLOCK COMBO. SEE DOCUMENTATION. */
+#define VIAL_INSECURE
